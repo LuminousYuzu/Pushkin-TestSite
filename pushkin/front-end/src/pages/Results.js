731f067b2e2f1a1ab820187a755d7b3a8b539f09
LuminousYuzu <yuzheliu007@outlook.com>
2024-10-22 19:25:32 -0400

    change for icon

--- pushkin/front-end/src/pages/Results.js
+++ pushkin/front-end/src/pages/Results.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+import { IoAccessibilityOutline, IoAccessibility } from "react-icons/io5";
 
 const ResultsPage = () => {
   const { rank } = useParams();
   const score = rank; 
-  const averageScore = 67.19; // Example average score, replace with actual data
+  const averageScore = 67.19; 
 
   // Calculate the number of people to highlight
   const totalPeople = 5;
   const highlightedPeople = Math.round((rank / 100) * totalPeople);
 
   const peopleIcons = Array.from({ length: totalPeople }, (_, i) => (
-    <div
-      key={i}
-      style={{
-        display: 'inline-block',
-        width: '20px',
-        height: '40px',
-        backgroundColor: i < highlightedPeople ? 'blue' : 'gray',
-        borderRadius: '10px',
-        margin: '0 5px'
-      }}
-    />
+    i < highlightedPeople 
+      ? <IoAccessibility key={i} style={{ margin: '0 5px', fontSize: '50px' }} /> 
+      : <IoAccessibilityOutline key={i} style={{ margin: '0 5px', fontSize: '50px' }} />
   ));
 
   return (
